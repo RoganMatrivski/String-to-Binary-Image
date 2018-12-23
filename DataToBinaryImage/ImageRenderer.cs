6e46e6eb71df89768bfddc3e29d18d1684bebc0f
Rogan Matrivski <robinmauritzrm@gmail.com>
2018-12-24 04:56:51 +0700

    Initial Commit

---
+++ DataToBinaryImage/ImageRenderer.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//using System.Drawing;
+//using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Diagnostics;
+
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+
+class ImageTooSmall : Exception
+{
+    public ImageTooSmall()
+    { }
+
+    public ImageTooSmall(string message) : base(message)
+    { }
+
+    public ImageTooSmall(string message, Exception inner) : base(message, inner)
+    { }
+
+
+}
+
+static class ImageRenderer
+{
+    private static byte GetBitsPerPixel(System.Drawing.Imaging.PixelFormat pixelFormat)
+    {
+        switch (pixelFormat)
+        {
+            case System.Drawing.Imaging.PixelFormat.Format24bppRgb:
+                return 24;
+                break;
+            case System.Drawing.Imaging.PixelFormat.Format32bppArgb:
+            case System.Drawing.Imaging.PixelFormat.Format32bppPArgb:
+            case System.Drawing.Imaging.PixelFormat.Format32bppRgb:
+                return 32;
+                break;
+            default:
+                throw new ArgumentException("Only 24 and 32 bit images are supported");
+
+        }
+    }
+
+
+    static byte valswitch(bool boolval, byte val)
+    {
+        if (boolval)
+            return val;
+        else
+            return 0;
+    }
+
+    [Obsolete("Don't use this.", true)]
+    public static unsafe System.Drawing.Image RendertoBWImg(byte[] src)
+    {
+        var bitdata = new BitArray(src);
+
+        int imgdimension = (int)Math.Ceiling(Math.Sqrt(bitdata.Length));
+
+        System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(imgdimension, imgdimension);
+
+        System.Drawing.Imaging.BitmapData bitmapdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+        byte bitsperpixel = GetBitsPerPixel(bitmapdata.PixelFormat);
+
+        byte* scan0 = (byte*)bitmapdata.Scan0.ToPointer();
+
+        for (int i = 0; i < bitmapdata.Height; i++)
+        {
+            for (int j = 0; j < bitmapdata.Width; j++)
+            {
+                if (j + (i * bitmapdata.Width) >= bitdata.Length)
+                {
+                    break;
+                }
+
+                byte* data = scan0 + i * bitmapdata.Stride + j * bitsperpixel / 8;
+
+                //Console.WriteLine($"Writepos {j + (i * bitmapdata.Width)}. J = {j}; I = {i}");
+
+                if (bitdata[j + (i * bitmapdata.Width)] == true)
+                {
+                    data[0] = 255;
+                    data[1] = 255;
+                    data[2] = 255;
+                }
+            }
+        }
+
+        bmp.UnlockBits(bitmapdata);
+
+        return bmp;
+    }
+
+    [Obsolete("Don't use this.", true)]
+    public static unsafe void SavetoBWImg(byte[] src, string name)
+    {
+        var bitdata = new BitArray(src);
+
+        int imgdimension = (int)Math.Ceiling(Math.Sqrt(bitdata.Length));
+
+        System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(imgdimension, imgdimension);
+
+        System.Drawing.Imaging.BitmapData bitmapdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+        byte bitsperpixel = GetBitsPerPixel(bitmapdata.PixelFormat);
+
+        byte* scan0 = (byte*)bitmapdata.Scan0.ToPointer();
+
+        for (int i = 0; i < bitmapdata.Height; i++)
+        {
+            for (int j = 0; j < bitmapdata.Width; j++)
+            {
+                if (j + (i * bitmapdata.Width) >= bitdata.Length)
+                {
+                    break;
+                }
+
+                byte* data = scan0 + i * bitmapdata.Stride + j * bitsperpixel / 8;
+
+                //Console.WriteLine($"Writepos {j + (i * bitmapdata.Width)}. J = {j}; I = {i}");
+
+                if (bitdata[j + (i * bitmapdata.Width)] == true)
+                {
+                    data[0] = 255;
+                    data[1] = 255;
+                    data[2] = 255;
+                }
+            }
+        }
+
+        bmp.UnlockBits(bitmapdata);
+
+        bmp.Save(name, System.Drawing.Imaging.ImageFormat.Bmp);
+    }
+
+    public static BitmapSource RenderToBWImg(byte[] src, int scaling = 1, bool reversed = false, int maxwidth = 0, int maxheight = 0)
+    {
+        if (src.Length == 0)
+            return null;
+
+        var bitdata = new BitArray(src);
+        //var bitdata = new BitArray(new bool[] { false, true, false, true, true, false, true, false, false, true, false, true, true, false, true, false });
+
+        int imgdimension = (int)Math.Ceiling(Math.Sqrt(bitdata.Length));
+
+        int stride = imgdimension;
+
+        if (imgdimension > maxwidth * maxheight)
+        {
+
+        }
+
+        int width = imgdimension;
+        int height = imgdimension;
+
+        if (maxwidth != 0 && maxheight == 0)
+        {
+            width = maxwidth;
+            height = (int)Math.Ceiling(bitdata.Length / (double)width);
+        }
+        
+        else if(maxheight != 0 && maxwidth == 0)
+        {
+            height = maxheight;
+            width = (int)Math.Ceiling(bitdata.Length / (double)height);}
+        
+        else if(maxheight != 0 && maxwidth != 0)
+        {
+            height = maxheight;
+            width = maxwidth;
+        }
+
+        stride = width;
+
+        //if (maxwidth != 0)
+        //{
+        //    width = maxwidth;
+        //    stride = width;
+
+        //    height = (int)Math.Ceiling(bitdata.Length / (double)width);
+        //}
+
+        //if (maxheight != 0)
+        //{
+        //    height = maxheight;
+        //}
+
+        if (width * height < bitdata.Length)
+            //throw new ;
+            throw new ImageTooSmall("Image is too small for the data");
+
+        #region Abandoned Code
+        //Abandoning this for a more better solution
+        //
+        //int stride = imgdimension * 4;
+
+        //byte[] pixel_data = new byte[imgdimension * imgdimension * 4];
+
+        //for (int i = 0; i < imgdimension; i++)
+        //{
+        //    for (int j = 0; j < imgdimension * 4; j += 4)
+        //    {
+        //        int datapos = j + (i * (imgdimension * 4));
+        //        int bitpos = (j / 4) + (i * imgdimension);
+
+        //        if (bitdata[bitpos])
+        //        {
+        //            pixel_data[datapos      ] = 255;
+        //            pixel_data[datapos + 1  ] = 255;
+        //            pixel_data[datapos + 2  ] = 255;
+        //            pixel_data[datapos + 3  ] = 255;
+        //        }
+        //        else
+        //        {
+        //            pixel_data[datapos + 3] = 255;
+        //        }
+        //    }
+        //}
+        #endregion
+
+        byte[] pixel_data = new byte[width * height];
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int pos = j + (i * width);
+
+                if (pos >= bitdata.Length)
+                    break;
+
+                //Yeah i know this is a silly one.
+                if (!reversed)
+                    if (bitdata[pos])
+                        pixel_data[pos] = 255;
+                    else
+                        pixel_data[pos] = 0;
+                else
+                    if (!bitdata[pos])
+                        pixel_data[pos] = 255;
+                    else
+                        pixel_data[pos] = 0;
+            }
+        }
+
+        var scaled = resizePixels(pixel_data, width, height, width * scaling, height * scaling);
+
+        return BitmapSource.Create(width * scaling, height * scaling, 300, 300, PixelFormats.Indexed8, BitmapPalettes.Gray256, scaled, stride * scaling);
+    }
+
+    //Using this code from this site : 
+    //http://tech-algorithm.com/articles/nearest-neighbor-image-scaling/
+    public static byte[] resizePixels(byte[] pixels, int _w1, int _h1, int _w2, int _h2)
+    {
+        byte[] temp = new byte[_w2 * _h2];
+
+        ulong w1 = (ulong)_w1;
+        ulong h1 = (ulong)_h1;
+        ulong w2 = (ulong)_w2;
+        ulong h2 = (ulong)_h2;
+
+        // EDIT: added +1 to account for an early rounding problem
+        ulong x_ratio = (ulong)((w1 << 16) / w2);// + 1;
+        ulong y_ratio = (ulong)((h1 << 16) / h2);// + 1;
+        //int x_ratio = (int)((w1<<16)/w2) ;
+        //int y_ratio = (int)((h1<<16)/h2) ;
+        ulong x2, y2;
+        for (ulong i = 0; i < h2; i++)
+        {
+            for (ulong j = 0; j < w2; j++)
+            {
+                x2 = ((j * x_ratio) >> 16);
+                y2 = ((i * y_ratio) >> 16);
+                temp[(i * w2) + j] = pixels[(y2 * w1) + x2];
+            }
+        }
+        return temp;
+    }
+}
---
+++ DataToBinaryImage/MainWindow.xaml.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+
+namespace DataToBinaryImage
+{
+    public class Update_Preview : INotifyPropertyChanged
+    {
+        // Declare the event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Update_Preview()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static string text_to_render;
+
+        private void update_preview(string string_text)
+        {
+            text_to_render = string_text;
+
+            if (ImageView == null || imgsize == null || imgsize_scaled == null || ScaleOption == null)
+                return;
+
+            if (ImageView != null)
+            {
+                var text = text_to_render.TrimEnd(new char[] { '\r', '\n' });
+
+                int width = 0;
+                int height = 0;
+
+                if (!string.IsNullOrWhiteSpace(MaxWidth.Text) && MaxWidth.Text != "0")
+                    width = Convert.ToInt32(MaxWidth.Text);
+
+                if (!string.IsNullOrWhiteSpace(MaxHeight.Text) && MaxHeight.Text != "0")
+                    height = Convert.ToInt32(MaxHeight.Text);
+
+                BitmapSource result;
+
+                string errmessage = "";
+
+                try
+                {
+                    result = ImageRenderer.RenderToBWImg(Encoding.UTF8.GetBytes(text), 1, invert_checkbox.IsChecked ?? false, width, height);
+                }
+                catch(ImageTooSmall ex)
+                {
+                    result = null;
+                    errmessage = "Image Too Small For Data";
+                }
+
+                if (result != null)
+                {
+                    ImageView.Source = result;
+
+                    imgsize.Content = $"Current Size : {result.PixelWidth}x{result.PixelHeight}";
+                    imgsize_scaled.Content = $"Current Scaled Size : {result.PixelWidth * ((scale_options)ScaleOption.SelectedItem).value}x{result.PixelHeight * ((scale_options)ScaleOption.SelectedItem).value}";
+                    status_label.Content = "";
+                }
+                else
+                {
+                    ImageView.Source = new BitmapImage(new Uri(@"/DataToBinaryImage;component/resource/broken.png", UriKind.Relative));
+
+                    imgsize.Content = $"Current Size : NaN";
+                    imgsize_scaled.Content = $"Current Scaled Size : NaN";
+                    status_label.Content = errmessage;
+                    
+                }
+            }
+        }
+
+        private void update_preview(object sender, RoutedEventArgs e)
+        {
+            update_preview(text_to_render);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int width = 0;
+            int height = 0;
+
+            if (!string.IsNullOrWhiteSpace(MaxWidth.Text) && MaxWidth.Text != "0")
+                width = Convert.ToInt32(MaxWidth.Text);
+
+            if (!string.IsNullOrWhiteSpace(MaxHeight.Text) && MaxHeight.Text != "0")
+                height = Convert.ToInt32(MaxHeight.Text);
+
+            //var res = ImageRenderer.RenderToBWImg(Encoding.UTF8.GetBytes(new TextRange(MessageBox.Document.ContentStart, MessageBox.Document.ContentEnd).Text));
+            var text = msg_string.Text.TrimEnd(new char[] { '\r', '\n'});
+
+            //var res = ImageRenderer.RenderToBWImg(Encoding.UTF8.GetBytes(text));
+            var res = ImageRenderer.RenderToBWImg(Encoding.UTF8.GetBytes(text), ((scale_options)ScaleOption.SelectedItem).value, invert_checkbox.IsChecked ?? false, width, height);
+
+            ShowExportDialog(res);
+        }
+
+        private void ShowExportDialog(BitmapSource image)
+        {
+            if (image == null) return;
+
+            SaveFileDialog saveImg = new SaveFileDialog();
+            saveImg.Filter = "JPEG Image|*.jpg|PNG Image|*.png|Bitmap Image|*.bmp|TIFF Image|*.tiff|Windows Media Bitmap|*.wmb";
+            saveImg.Title = "Export Single Image";
+            saveImg.FileName = "Export";
+            saveImg.AddExtension = true;
+            saveImg.DefaultExt = "*.png";
+
+            saveImg.CheckPathExists = true;
+
+            BitmapEncoder encoder = null;
+            if (saveImg.ShowDialog() == true)
+            {
+                string ext = Path.GetExtension(saveImg.FileName);
+                switch (ext.ToLower())
+                {
+                    case ".png":
+                        encoder = new PngBitmapEncoder();
+                        break;
+                    case ".bmp":
+                        encoder = new BmpBitmapEncoder();
+                        break;
+                    case ".jpeg":
+                    case ".jpg":
+                        encoder = new JpegBitmapEncoder();
+                        break;
+                    case ".tiff":
+                        encoder = new TiffBitmapEncoder();
+                        break;
+                    case ".wmb":
+                        encoder = new WmpBitmapEncoder();
+                        break;
+                    default:
+                        encoder = new BmpBitmapEncoder();
+                        saveImg.FileName += ".bmp";
+                        break;
+                }
+
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                try
+                {
+                    using (FileStream fs = new FileStream(saveImg.FileName, FileMode.Create))
+                    {
+                        encoder.Save(fs);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Export file failed. See details of the error below :" +
+                        Environment.NewLine +
+                        Environment.NewLine +
+                        $"{ex.Message}", "Error!", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+
+                status_label.Content = "Export Completed!";
+            }
+        }
+
+
+        private void msg_string_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            update_preview(msg_string.Text);
+        }
+
+        private void ScaleOption_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int test = (int)Math.Pow(2, i);
+                ScaleOption.Items.Add(new scale_options((int)Math.Pow(2, i), Math.Pow(2, i) + "x"));
+            }
+
+            ScaleOption.SelectedIndex = 0;
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void MaxHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            update_preview(msg_string.Text);
+        }
+
+        private void MaxHeight_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void MaxWidth_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void MaxWidth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            update_preview(msg_string.Text);
+        }
+
+        private static Task runningtask;
+
+        private void MainForm_Loaded(object sender, RoutedEventArgs e)
+        {
+            runningtask = Task.Run(async () =>
+            {
+                char[] charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+                Random rand = new Random();
+
+                while (true)
+                {
+                    //this.Dispatcher.Invoke(() => MaxWidth.Text = rand.Next(2, 32).ToString());
+
+                    int remaining = rand.Next(8, 64);
+                    while (remaining-- > 0)
+                    {
+
+                        int word = rand.Next(4, 10);
+                        while (word-- > 0)
+                        {
+                            this.Dispatcher.Invoke(() => msg_string.Text += charset[rand.Next(charset.Length)]);
+                            await Task.Delay(3);
+
+                        }
+
+                        this.Dispatcher.Invoke(() => msg_string.Text += " ");
+
+                        if (rand.NextDouble() > 0.85)
+                            this.Dispatcher.Invoke(() => msg_string.Text += "\n");
+                    }
+                    this.Dispatcher.Invoke(() => msg_string.Text = "");
+                }
+            });
+        }
+
+        private async void seq_save_Click(object sender, RoutedEventArgs e)
+        {
+            int width = 0;
+            int height = 0;
+
+            if (!string.IsNullOrWhiteSpace(MaxWidth.Text) && MaxWidth.Text != "0")
+                width = Convert.ToInt32(MaxWidth.Text);
+
+            if (!string.IsNullOrWhiteSpace(MaxHeight.Text) && MaxHeight.Text != "0")
+                height = Convert.ToInt32(MaxHeight.Text);
+
+            var text = msg_string.Text.TrimEnd(new char[] { '\r', '\n' });
+
+            List<string> stringsequenced = new List<string>();
+
+            for (int i = 1; i <= text.Length; i++)
+            {
+                char[] container = new char[i];
+
+                for (int x = 0; x < i; x++)
+                {
+                    container[x] = text[x];
+                }
+
+                stringsequenced.Add(new string(container));
+            }
+
+            await Task.Run(() =>
+            {
+                int counter = 1;
+                foreach (var subtext in stringsequenced)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        msg_string.Text = subtext;
+
+                        var res = ImageRenderer.RenderToBWImg(Encoding.UTF8.GetBytes(subtext), ((scale_options)ScaleOption.SelectedItem).value, invert_checkbox.IsChecked ?? false, width, height);
+
+                        PngBitmapEncoder pngencoder = new PngBitmapEncoder();
+
+                        pngencoder.Frames.Add(BitmapFrame.Create(res));
+
+                        using (var stream = new FileStream($"Test_{counter++}.png", FileMode.Create))
+                            pngencoder.Save(stream);
+                    });
+                }
+            });
+        }
+    }
+
+    public struct scale_options
+    {
+        public int value;
+        public string text;
+
+        public scale_options(int _value, string _text)
+        {
+            value = _value;
+            text = _text;
+        }
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+}
